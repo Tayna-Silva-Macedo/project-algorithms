5f390a3b8404c67537c40fc245f7dd21d10f21a8
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-02-16 18:46:07 -0300

    feat: Implementação da função find_diplicate, que retorna o numero que está duplicado em uma lista ou False

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,29 @@
+from collections import Counter
+
+
+def is_valid_list(array):
+    return array and isinstance(array, list)
+
+
+def is_all_numbers(array):
+    return all((isinstance(number, int)) for number in array)
+
+
+def is_all_positive(array):
+    return all((number > 0) for number in array)
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (
+        not is_valid_list(nums)
+        or not is_all_numbers(nums)
+        or not is_all_positive(nums)
+    ):
+        return False
+
+    number, quantity = Counter(nums).most_common()[0]
+
+    if quantity == 1:
+        return False
+
+    return number
