1239b58029aae8f4a67f0e4919372e190a505799
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-02-16 16:22:48 -0300

    feat: Implementação da função is_palindrome_recursive, que retorna se uma palavra é palíndroma ou não

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
+def reverse(word: str):
+    if len(word) == 1:
+        return word
+    else:
+        return reverse(word[1:]) + word[0]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    word_reversed = reverse(word)
+    return word_reversed.lower() == word.lower()
